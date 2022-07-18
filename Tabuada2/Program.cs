77bae90818c48e9c097c2bb04b9cfdb7eec06734
kamui-7 <gabrielmarinsrodrigues@gmail.com>
2022-07-18 10:15:12 -0300

    Lista 4 - Tabuada

---
+++ Tabuada2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabuada2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, produto;
+
+            Console.Write("Deseja saber a tabuada de qual numero? ");
+            n = int.Parse(Console.ReadLine());
+
+            for(i = 0; i <= 10; i++)
+            {
+                produto = n * i;
+                Console.WriteLine(n + " X " + i + " = " + produto);
+            }
+        }
+    }
+}
